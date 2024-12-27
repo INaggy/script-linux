452673d740f76b6b94ba4d71b4e4e09657b6a1f9
Naggy <113348272+INaggy@users.noreply.github.com>
2024-12-27 17:55:48 +0300

    Delete create_installer.sh

--- create_installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Создание установщика..."
-# Пример: упаковать проект в архив
-OUTPUT_DIR="installer"
-mkdir -p "$Projects_"
-tar -czf "$Projects_/project_installer.tar.gz" .
-echo "Установщик создан: $Projects_/project_installer.tar.gz"
